c7105d286a027c707fa84d72589d8b67e3d43929
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-23 15:05:03 -0400

    Code Along Scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css("section.column-highlights:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > h3:nth-child(2)")
+
+
